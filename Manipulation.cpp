0f5205b6e7fd0e4ce6af371d7fa69b70aefafc09
TheRareKid <mirswad.chikku@gmail.com>
2021-04-16 10:38:59 +0530

    Manipulation operators

---
+++ Manipulation.cpp
@@ -0,0 +1,15 @@
+ //Program for showing manipulation operators
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int  main()
+{
+    int m,n,p;
+    m = 2597;
+    n = 14;
+    p =175;
+    cout<<setw(10) <<"First=" <<setw(10) << p  << endl;
+    cout<<setw(10) <<"Second=" <<setw(10) << p  << endl;
+    cout<<setw(10) <<"Third=" <<setw(10) << m <<endl;
+    return 0;
+}
