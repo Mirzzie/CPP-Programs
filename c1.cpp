617142d22f077ffcc2146771373b1a6c516e74ac
TheRareKid <mirswad.chikku@gmail.com>
2021-04-20 22:03:07 +0530

    sample

---
+++ c1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter two integers:";
+    cin>>num;
+    if(num%2==0)
+    {
+      cout<<"its even";
+    }
+    else
+    cout<<"Old";
+    return 0;
+}
